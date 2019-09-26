89683844ab4f028f72ebb1ed1fb22078f70eb211
salty68 <lck295@nyu.edu>
2019-09-26 17:12:17 -0400

    InitCommit
    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player cleanMan;
+
+    float horizontal;
+    float vertical;
+
+    float runSpeed = 5f;
+
+    public  int currentTrash = 0;
+    public  int trashLimit = 5;
+
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        cleanMan = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidBody.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Trash" && currentTrash < 5)
+        {
+            Destroy(collision.gameObject);
+            currentTrash += 1;
+        }
+
+        if(collision.gameObject.tag == "Trash Can")
+        {
+            currentTrash = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Trash_Holder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trash_Holder : MonoBehaviour
+{
+    string trashIndicator;
+    Text wordText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wordText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trashIndicator = Player.cleanMan.currentTrash + "/" + Player.cleanMan.trashLimit;
+        wordText.text = trashIndicator;
+    }
+}
